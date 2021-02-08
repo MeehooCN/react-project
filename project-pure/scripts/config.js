4f311fb9446caec945986d3e41f83132cad35725
obf1313 <oubaifan@163.com>
2021-02-08 16:26:29 +0800

    细化服务器配置文件注释

--- project-pure/scripts/config.js
+++ project-pure/scripts/config.js
@@ -4,11 +4,11 @@
  * @createTime: 2021/2/8 10:30
  **/
 /**
- * @host 服务器 IP 地址
- * @user 服务器用户名
- * @password 服务器密码
- * @path 打包上传地址，需精确到文件路径
- * @shPath 脚本目录
+ * @host 服务器ip地址 '127.0.0.1'
+ * @user 服务器用户名 'root'
+ * @password 服务器密码 '123456'
+ * @path 打包上传地址 '/opt/view/dist.zip'
+ * @shPath 脚本目录 '/opt/sh'
  **/
 module.exports = {
   host: '',
