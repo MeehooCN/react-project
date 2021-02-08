31f5833d1af1e6d0dcc1d86aca9d5135ba899e80
obf1313 <oubaifan@163.com>
2021-02-08 16:20:34 +0800

    配置文件注释

--- project-base/scripts/config.js
+++ project-base/scripts/config.js
@@ -3,7 +3,13 @@
  * @author: cnn
  * @createTime: 2021/2/8 10:30
  **/
-// path 需精确到文件路径
+/**
+ * @host 服务器 IP 地址
+ * @user 服务器用户名
+ * @password 服务器密码
+ * @path 打包上传地址，需精确到文件路径
+ * @shPath 脚本目录
+ **/
 module.exports = {
   host: '',
   user: '',
