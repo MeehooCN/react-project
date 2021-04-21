94ce31328907d7a7e0a07da0e796e720254f33a0
obf1313 <oubaifan@163.com>
2021-04-21 13:47:43 +0800

    linux 服务器上备份文件

--- project-template/scripts/deploy.sh
+++ project-template/scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # 此文件仅供参考
-# 进入放置 deploy.sh 的目录
+# 进入放置项目的目录
 cd /opt/view
+# 新建日期
+currentDate=$(date +%Y%m%d%H%M)
+# 新建备份目录
+mkdir -p /opt/backup/$currentDate
+# 备份原目录
+cp -r /opt/view/company_share /opt/backup/$currentDate/company_share
 # 删除原目录
 rm -rf company_share
 # 解压
