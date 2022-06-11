dc27da50942272da53ed61b0cbac435979d8550a
obf1313 <oubaifan@163.com>
2022-06-11 21:11:36 +0800

    添加 gitignore, lock, stylelint。

--- project-base/build/webpack.dev.config.js
+++ project-base/build/webpack.dev.config.js
@@ -4,12 +4,24 @@
  * @createTime: 2021/4/22 14:28
  **/
 const { merge } = require('webpack-merge');
+// 自动化 stylelint
+const StylelintPlugin = require('stylelint-webpack-plugin');
 const commonConfig = require('./webpack.config');
 const styleRules = require('./rules/styleRules');
+const { resolve } = require('./utils');
 
 module.exports = () => {
   const config = merge(commonConfig, {
-    devtool: 'cheap-module-source-map'
+    devtool: 'cheap-module-source-map',
+    plugins: [new StylelintPlugin({
+      configFile: resolve('.stylelintrc.json'),
+      extensions: ['less'],
+      files: '**/*.less',
+      fix: true,
+      customeSyntax: 'postcss-less',
+      lintDirtyModulesOnly: true,
+      threads: true
+    })]
   });
   for (let item of styleRules) {
     item.use[0] = 'style-loader';
